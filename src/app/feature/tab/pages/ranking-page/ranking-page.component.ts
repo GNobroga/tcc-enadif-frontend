d9b6bc7ebc38ad57d425743b06497283ac4564ca
gabiroba <gabrielcardosogirarde@gmail.com>
2024-11-11 07:24:16 -0300

    feat: Ranking

--- src/app/feature/tab/pages/ranking-page/ranking-page.component.ts
+++ src/app/feature/tab/pages/ranking-page/ranking-page.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
 import { AnimationController, ViewDidEnter } from '@ionic/angular';
 import RankingService, { UserRanking } from 'src/app/core/services/ranking.service';
 
@@ -14,13 +15,49 @@ export class RankingPageComponent implements ViewDidEnter,  AfterViewInit {
 
   rankings: Array<UserRanking> = [];
 
+  currentUser = this.auth.currentUser!;
+
   constructor(
     readonly animationController: AnimationController,
     readonly rankingService: RankingService,
+    readonly auth: Auth,
   ) { }
 
   ionViewDidEnter() {
      this.rankingService.listAll().subscribe(data => this.rankings = data);
+  }
+
+  // Index 0 is the first ranking
+  getUserRanking(index: number): UserRanking | null {
+    if (!this.rankings.length) return null;
+    if (index < 0 || index >= this.rankings.length) return null;
+    return this.rankings[index];
+  }
+
+  get firstUserRanking() {
+    return this.getUserRanking(0);
+  }
+
+  get secondUserRanking() {
+    return this.getUserRanking(1);
+  }
+
+  get thirdUserRanking() { 
+    return this.getUserRanking(3);
+  }
+
+  getFirstName(name: string) {
+    if (!name) return '';
+    return name.split(' ')[0];
+  }
+
+  isLoggedUser(userId: string) {
+    if (!userId) return false;
+    return userId === this.currentUser.uid;
+  }
+
+  get hasTopRankings() {
+    return this.firstUserRanking || this.secondUserRanking || this.thirdUserRanking;
   }
 
   ngAfterViewInit(): void {
